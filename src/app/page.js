1b5f0ad60494692083fdd93b9f9825c68dfd1906
Muhammad Awais <engineerawais1281@gmail.com>
2025-03-29 01:51:16 +0500

    v4

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ import Reservation from './component/Reservation'
 
 function page() {
   return (
-    <div className='bg-white'>
+    <div className='bg-white text-black'>
       <Hero/>
       <Services/>
       <About/>
